68706ff0a2d26b75ea90cfd15bf2f889aec7239e
Hauke Lauth <haukelauth@googlemail.com>
2024-09-18 11:10:40 +0200

    refactoring and some styling

--- pages/index.js
+++ pages/index.js
@@ -1,215 +1,30 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import initialItems from "@/public/assets/shopping-items.json";
 import categories from "@/public/assets/categories.json";
-import { nanoid } from "nanoid";
 import useLocalStorageState from "use-local-storage-state";
+import {
+  StyledButton,
+  StyledButtonContainer,
+  StyledConfirmationDialog,
+  StyledErrorMessage,
+  StyledInput,
+  StyledLabel,
+  StyledShoppingList,
+  StyledSelect,
+  StyledFormContainer,
+  StyledItemCard,
+  StyledItemDetailContainer,
+} from "@/components/StyledComponents";
+import {
+  handleFormData,
+  updateItemsState,
+  scrollToPurchasedItems,
+  handleCheckboxClick,
+  handleUnmarkClick,
+} from "@/components/functions";
 
-const StyledShoppingList = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 20px;
-
-  .items-container {
-    max-height: 400px;
-    overflow-y: auto;
-    border: 1px solid #ccc;
-    padding: 10px;
-  }
-`;
-
-const EditButton = styled.button`
-  background-color: #007bff;
-  color: white;
-  padding: 10px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  margin-right: 10px;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`;
-
-const PurchasedItemsContainer = styled.div`
-  max-height: 300px;
-  overflow-y: auto;
-  border: 1px solid #ccc;
-  padding: 10px;
-  margin-top: 20px;
-
-  h2 {
-    color: grey;
-  }
-
-  .purchased-item {
-    text-decoration: line-through;
-    color: grey;
-  }
-`;
-
-const ItemDetailContainer = styled.div`
-  max-height: 400px;
-  overflow-y: auto;
-  border: 1px solid #ccc;
-  padding: 10px;
-`;
-
-const ErrorMessage = styled.div`
-  color: red;
-  margin-bottom: 10px;
-`;
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 5px;
-  font-weight: bold;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 8px;
-  margin-bottom: 15px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-`;
-
-const Select = styled.select`
-  width: 100%;
-  padding: 8px;
-  margin-bottom: 15px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-`;
-
-const Button = styled.button`
-  background-color: #007bff;
-  color: white;
-  padding: 10px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  margin: 10px;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`;
-
-const ItemCard = styled.div`
-  display: block;
-  text-decoration: none;
-  color: inherit;
-  border: 1px solid #ddd;
-  padding: 10px;
-  margin-bottom: 10px;
-  border-radius: 5px;
-  background-color: ${({ category }) => {
-    switch (category) {
-      case "fruits":
-        return "#79a17d";
-      case "bakery":
-        return "#d4a76a";
-      case "dairy":
-        return "#7c9fb0";
-      case "vegetables":
-        return "#c07676";
-      case "meat":
-        return "#9d8bb5";
-      case "beverage":
-        return "#a0877d";
-      case "snacks":
-        return "#c299b0";
-      case "household":
-        return "#999999";
-      case "personal care":
-        return "#b1b179";
-      case "other":
-        return "#fc9997";
-      default:
-        return "#7ca6a6";
-    }
-  }};
-
-  &:hover {
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
-`;
-
-const FormContainer = styled.form`
-  margin-bottom: 20px;
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-
-  label {
-    display: block;
-    margin-bottom: 5px;
-  }
-
-  input,
-  select {
-    width: 100%;
-    padding: 8px;
-    margin-bottom: 10px;
-    border-radius: 5px;
-    border: 1px solid #ddd;
-  }
-
-  button {
-    padding: 10px;
-    background-color: #007bff;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    margin: 10px;
-
-    &:hover {
-      background-color: #0056b3;
-    }
-  }
-
-  .error {
-    color: red;
-    margin-bottom: 10px;
-  }
-`;
-
-const DeleteButton = styled.button`
-  background-color: #007bff;
-  color: white;
-  padding: 10px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  margin: 10px;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`;
-
-const ConfirmationDialog = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 20px;
-  background-color: white;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  text-align: center;
-
-  button {
-    margin: 10px;
-  }
-`;
-
-const ShoppingList = () => {
+function ShoppingList() {
   const [items, setItems] = useLocalStorageState("shoppingItems", []);
   const [purchasedItems, setPurchasedItems] = useLocalStorageState(
     "purchasedItems",
@@ -225,28 +40,16 @@ const ShoppingList = () => {
     0
   );
 
+  const topRef = useRef(null);
+
+  function scrollToTop() {
+    topRef.current?.scrollIntoView({ behavior: "smooth" });
+  }
+
   const TotalPurchased = (purchasedItems || []).reduce(
     (total, item) => total + item.quantity,
     0
   );
-
-  const handleCheckboxClick = (itemId) => {
-    const item = items.find((item) => item.id === itemId);
-    if (item) {
-      const purchasedItem = { ...item, purchased: true };
-      setPurchasedItems([purchasedItem, ...(purchasedItems || [])]);
-      setItems(items.filter((item) => item.id !== itemId));
-    }
-  };
-
-  const handleUnmarkClick = (itemId) => {
-    const purchasedItem = purchasedItems.find((item) => item.id === itemId);
-    if (purchasedItem) {
-      const item = { ...purchasedItem, purchased: false };
-      setItems([item, ...(items || [])]);
-      setPurchasedItems(purchasedItems.filter((item) => item.id !== itemId));
-    }
-  };
 
   useEffect(() => {
     if ((items || []).length === 0) {
@@ -258,35 +61,12 @@ const ShoppingList = () => {
     }
   }, []);
 
-  const handleFormData = (form) => {
-    const formData = new FormData(form);
-    const newItem = Object.fromEntries(formData.entries());
-    newItem.quantity = parseInt(newItem.quantity) || 1;
-
-    const formErrors = validateForm(newItem);
-    return { newItem, formErrors };
-  };
-
-  const validateForm = (item) => {
-    const errors = {};
-    if (!item.name) errors.name = "Name is required";
-    if (item.quantity <= 0)
-      errors.quantity = "Quantity must be a positive number";
-    if (!item.category || item.category === "default")
-      errors.category = "Please select a category";
-    return errors;
-  };
-
-  const updateItemsState = (newItem) => {
-    const updatedItems = [{ ...newItem, id: nanoid() }, ...items];
-    setItems(updatedItems);
-  };
-
-  const handleEdit = (item) => {
+  function handleEdit(item) {
     setEditingItem(item);
     setIsFormVisible(true);
-  };
-  const handleSubmit = (e) => {
+  }
+
+  function handleSubmit(e) {
     e.preventDefault();
     const { newItem, formErrors } = handleFormData(e.target);
     if (Object.keys(formErrors).length > 0) {
@@ -294,71 +74,88 @@ const ShoppingList = () => {
     } else {
       setErrors({});
       if (editingItem) {
-        const updatedItems = items.map((item) =>
-          item.id === editingItem.id ? { ...newItem, id: item.id } : item
-        );
+        const updatedItems = items.map(function (item) {
+          return item.id === editingItem.id
+            ? { ...newItem, id: item.id }
+            : item;
+        });
         setItems(updatedItems);
         setEditingItem(null);
       } else {
-        updateItemsState(newItem);
+        setItems(updateItemsState(newItem, items));
       }
       e.target.reset();
       setIsFormVisible(false);
     }
-  };
+  }
 
-  const handleDelete = (id) => {
+  function handleDelete(id) {
     setDeletingItemId(id);
-  };
+  }
 
-  const confirmDelete = () => {
+  function confirmDelete() {
     setItems(items.filter((item) => item.id !== deletingItemId));
     setDeletingItemId(null);
-  };
+  }
 
-  const cancelDelete = () => {
+  function cancelDelete() {
     setDeletingItemId(null);
-  };
+  }
 
   return (
     <StyledShoppingList>
+      <div ref={topRef}></div>
       <h1>Shopping List: What I Need to Buy</h1>
 
       {!isFormVisible && (
-        <Button
-          onClick={() => {
-            setEditingItem(null);
-            setIsFormVisible(true);
-          }}
-        >
-          Add New Item
-        </Button>
+        <StyledButtonContainer>
+          <StyledButton
+            onClick={() => {
+              setEditingItem(null);
+              setIsFormVisible(true);
+            }}
+          >
+            Add New Item
+          </StyledButton>
+
+          {TotalPurchased > 0 && (
+            <StyledButton onClick={scrollToPurchasedItems}>
+              Purchased Items ({TotalPurchased})
+            </StyledButton>
+          )}
+        </StyledButtonContainer>
       )}
 
       {isFormVisible && (
-        <FormContainer onSubmit={handleSubmit} id="add-item-form">
+        <StyledFormContainer onSubmit={handleSubmit} id="add-item-form">
           <h2>
             {editingItem ? "Edit Shopping Item" : "Add New Shopping Item"}
           </h2>
-          {errors.name && <ErrorMessage>{errors.name}</ErrorMessage>}
-          <Label htmlFor="name">Item Name</Label>
-          <Input
+          {errors.name && (
+            <StyledErrorMessage>{errors.name}</StyledErrorMessage>
+          )}
+          <StyledLabel htmlFor="name">Item Name</StyledLabel>
+          <StyledInput
             type="text"
             id="name"
             name="name"
             defaultValue={editingItem?.name || ""}
           />
-          {errors.quantity && <ErrorMessage>{errors.quantity}</ErrorMessage>}
-          <Label htmlFor="quantity">Quantity</Label>
-          <Input
+          {errors.quantity && (
+            <StyledErrorMessage>{errors.quantity}</StyledErrorMessage>
+          )}
+          <StyledLabel htmlFor="quantity">Quantity</StyledLabel>
+          <StyledInput
             type="number"
             id="quantity"
             name="quantity"
             defaultValue={editingItem?.quantity || ""}
           />
-          {errors.category && <ErrorMessage>{errors.category}</ErrorMessage>}
-          <Label htmlFor="category">Category</Label>
-          <Select
+          {errors.category && (
+            <StyledErrorMessage>{errors.category}</StyledErrorMessage>
+          )}
+          <StyledLabel htmlFor="category">Category</StyledLabel>
+          <StyledSelect
             id="category"
             name="category"
             defaultValue={editingItem?.category || "default"}
@@ -369,22 +166,22 @@ const ShoppingList = () => {
                 {category}
               </option>
             ))}
-          </Select>
-          <Label htmlFor="comment">Comment</Label>
-          <Input
+          </StyledSelect>
+          <StyledLabel htmlFor="comment">Comment</StyledLabel>
+          <StyledInput
             type="text"
             id="comment"
             name="comment"
             placeholder="Optional"
             defaultValue={editingItem?.comment || ""}
           />
-          <EditButton type="submit">
+          <StyledButton type="submit">
             {editingItem ? "Save Changes" : "Add Item"}
-          </EditButton>
-          <Button type="reset" onClick={() => setIsFormVisible(false)}>
+          </StyledButton>
+          <StyledButton type="reset" onClick={() => setIsFormVisible(false)}>
             Cancel
-          </Button>
-        </FormContainer>
+          </StyledButton>
+        </StyledFormContainer>
       )}
 
       {totalItems === 0 && TotalPurchased > 0 && (
@@ -395,14 +192,9 @@ const ShoppingList = () => {
       ) : (
         <>
           <p>Total items: {totalItems}</p>
-          <ItemDetailContainer>
+          <StyledItemDetailContainer>
             {items.map((item) => (
-              <ItemCard key={item.id} category={item.category}>
-                <input
-                  type="checkbox"
-                  checked={item.purchased || false}
-                  onChange={() => handleCheckboxClick(item.id)}
-                />
+              <StyledItemCard key={item.id} category={item.category}>
                 <Link key={item.id} href={`/item/${item.id}`} passHref>
                   <div>
                     <h3>{item.name}</h3>
@@ -415,51 +207,78 @@ const ShoppingList = () => {
                     </p>
                   </div>
                 </Link>
-                <EditButton onClick={() => handleEdit(item)}>Edit</EditButton>
-                <DeleteButton onClick={() => handleDelete(item.id)}>
+                <StyledButton onClick={() => handleEdit(item)}>
+                  Edit
+                </StyledButton>
+                <StyledButton onClick={() => handleDelete(item.id)}>
                   Delete
-                </DeleteButton>
-              </ItemCard>
+                </StyledButton>
+                <input
+                  type="checkbox"
+                  title="Click to mark as purchased"
+                  checked={item.purchased || false}
+                  onChange={() =>
+                    handleCheckboxClick(
+                      item.id,
+                      items,
+                      purchasedItems,
+                      setItems,
+                      setPurchasedItems
+                    )
+                  }
+                />
+                Click to mark as purchased
+              </StyledItemCard>
             ))}
-          </ItemDetailContainer>
 
-          {TotalPurchased > 0 && (
-            <PurchasedItemsContainer>
-              <h2>Purchased Items</h2>
-              <p>Total purchased items: {TotalPurchased}</p>
-              {purchasedItems.map((item) => (
-                <ItemCard
-                  key={item.id}
-                  category={item.category}
-                  className="purchased-item"
-                >
-                  <input
-                    type="checkbox"
-                    checked={item.purchased || false}
-                    onChange={() => handleUnmarkClick(item.id)}
-                  />
-                  <div>
-                    <h3 style={{ textDecoration: "line-through" }}>
-                      {item.name}
-                    </h3>
-                    <p>Quantity: {item.quantity}</p>
-                  </div>
-                </ItemCard>
-              ))}
-            </PurchasedItemsContainer>
-          )}
+            {TotalPurchased > 0 && (
+              <div id="purchased-items">
+                <h2>Purchased Items</h2>
+                <StyledButton onClick={scrollToTop}>Back to Top</StyledButton>
+                <p>Total purchased items: {TotalPurchased}</p>
+                {purchasedItems.map((item) => (
+                  <StyledItemCard
+                    key={item.id}
+                    category={item.category}
+                    className="purchased-item"
+                  >
+                    <div>
+                      <h3 style={{ textDecoration: "line-through" }}>
+                        {item.name}
+                      </h3>
+                      <p>Quantity: {item.quantity}</p>
+                    </div>
+                    <input
+                      type="checkbox"
+                      checked={item.purchased || false}
+                      onChange={() =>
+                        handleUnmarkClick(
+                          item.id,
+                          items,
+                          purchasedItems,
+                          setItems,
+                          setPurchasedItems
+                        )
+                      }
+                    />
+                    Click again to unmark
+                  </StyledItemCard>
+                ))}
+              </div>
+            )}
+          </StyledItemDetailContainer>
         </>
       )}
 
       {deletingItemId && (
-        <ConfirmationDialog>
+        <StyledConfirmationDialog>
           <p>Are you sure you want to delete this item?</p>
-          <Button onClick={confirmDelete}>Yes, delete</Button>
-          <Button onClick={cancelDelete}>Cancel</Button>
-        </ConfirmationDialog>
+          <StyledButton onClick={confirmDelete}>Yes, delete</StyledButton>
+          <StyledButton onClick={cancelDelete}>Cancel</StyledButton>
+        </StyledConfirmationDialog>
       )}
     </StyledShoppingList>
   );
-};
+}
 
 export default ShoppingList;
